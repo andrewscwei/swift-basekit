283d088ab7790b1a6d4d1adfc866961cd6d48bff
Andrew Wei <andrewscwei@gmail.com>
2021-07-29 23:20:41 -0700

    Update build pipeline

--- Package.swift
+++ Package.swift
@@ -2,6 +2,33 @@
 
 import PackageDescription
 
+#if os(Linux)
+import Glibc
+#else
+import Darwin.C
+#endif
+
+enum Environment: String {
+  static let `default`: Environment = .local
+
+  case local
+  case development
+  case production
+
+  static func get() -> Environment {
+    if let envPointer = getenv("CI"), String(cString: envPointer) == "true" {
+      return .production
+    }
+    else if let envPointer = getenv("SWIFT_ENV") {
+      let env = String(cString: envPointer)
+      return Environment(rawValue: env) ?? .default
+    }
+    else {
+      return .default
+    }
+  }
+}
+
 let package = Package(
   name: "BaseKit",
   platforms: [.iOS(.v11)],
