3f326942a8c194b3bdc3d17b6bcb2e2c00c006b3
Andrew Wei <andrewscwei@gmail.com>
2021-08-18 11:13:51 -0700

    Add isEnabled flag to log

--- Sources/BaseKit/tool/log.swift
+++ Sources/BaseKit/tool/log.swift
@@ -7,12 +7,16 @@ import Foundation
 ///
 /// - Parameters:
 ///   - level: The log level.
-///   - isPublic: Indicates if the log is publicly accessible.
+///   - isPublic: Specifies if the log is publicly accessible.
+///   - isEnabled: Specifies if the log is enabled, a convenient way to skip this log based on some
+///                external flag.
 ///   - fileName: Name of the file where this function was called.
 ///   - functionName: Name of the function where this function was called.
 ///   - lineNumber: Line number where this function was called.
 ///   - message: The block that returns the message.
-public func log(_ level: OSLogType = .info, isPublic: Bool = true, fileName: String = #file, functionName: String = #function, lineNumber: Int = #line, message: () -> String) {
+public func log(_ level: OSLogType = .info, isPublic: Bool = true, isEnabled: Bool = true, fileName: String = #file, functionName: String = #function, lineNumber: Int = #line, message: () -> String) {
+  if !isEnabled { return }
+
   #if DEBUG
 
   let fileName = fileName.components(separatedBy: "/").last?.components(separatedBy: ".").first
