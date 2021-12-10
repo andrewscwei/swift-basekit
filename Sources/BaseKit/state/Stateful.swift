ca27bf43a5c481ede99252851eaaba674c107768
Andrew Wei <andrew@sybl.io>
2021-12-10 13:01:47 -0800

    Optimizations

--- Sources/BaseKit/state/Stateful.swift
+++ Sources/BaseKit/state/Stateful.swift
@@ -7,23 +7,23 @@ import Foundation
 /// emits an update event to the `StateMachineDelegate`, where the update cycle can be handled. The
 /// wrapped property can be an optional type.
 ///
-/// When transforming this property into a state, you can optionally assign more a `StateType` to it
-/// by specifying the first parameter of this property wrapper. When the value of the state is
+/// When transforming this property into a state, you can optionally assign one or more `StateType`s
+/// to it by specifying the first parameter of this property wrapper. When the value of the state is
 /// changed, its key path (relative to the property owner) is marked as dirty along with all its
 /// associated state types.
 ///
-/// To conditionally prevent a new value to be assigned, you can specify the `willSet` parameter, a
-/// block with the old value and new value as arguments, which must be satisfied whenever a new
-/// value is assigned to the state. If the block returns `true`, the new value will be assigned. If
-/// `false`, the new value will not be assigned. Note that in either case, Swift's built-in `didSet`
-/// block will still be triggered for the wrapped property.
+/// To conditionally prevent a new value from being assigned, you can provide the `willSet`
+/// parameter, a block with the old value and new value as arguments, which must be satisfied
+/// whenever a new value is assigned to the state. If the block returns `true`, the new value will
+/// be assigned. If `false`, the new value will not be assigned. Note that in either case, Swift's
+/// native `didSet` block will still be triggered for the wrapped property.
 ///
 /// To handle the event when a new value is successfully assigned, you can provide the `didSet`
 /// parameter, which is a block consisting of the old value and the new value as arguments. This
 /// parameter should not be confused with the built-in `didSet` block of Swift properties. The
 /// difference is that the `didSet` parameter of this property wrapper only triggers when the state
 /// value has changed, whereas the built-in `didSet` block gets triggered regardless of whether the
-/// value was changed. You can omit this parameter and achieve the same behavior with the built-in
+/// value was changed. You can omit this parameter and achieve the same behavior with the native
 /// `didSet` block by utilizing the `projectedValue` of this property wrapper and checking the
 /// `isDirty` flag:
 ///
@@ -37,6 +37,8 @@ import Foundation
 ///   }
 /// }
 /// ```
+///
+/// Note that the native `didSet` block is triggered **after** the update cycle is resolved.
 ///
 /// **Warning**: This property wrapper uses private API and causes segmentation fault in the
 /// compiler if the type constraint of the property owner is not satisfied.
