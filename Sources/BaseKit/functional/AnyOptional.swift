87e4862d0ea67b5b1fb1e0ec4a2a91db419478c5
Andrew Wei <andrewscwei@gmail.com>
2022-04-10 10:38:50 -0700

    Update documentation

--- Sources/BaseKit/functional/AnyOptional.swift
+++ Sources/BaseKit/functional/AnyOptional.swift
@@ -2,8 +2,8 @@
 
 import Foundation
 
-/// Protocol exposing a property for a `ExpressibleByNilLiteral` value that checks if the value is
-/// `nil`.
+/// An `ExpressibleByNilLiteral` value conforming to the `AnyOptional` protocol implements a
+/// property that indicates if it is equal to `nil`.
 public protocol AnyOptional: ExpressibleByNilLiteral {
 
   /// Indicates if the value is `nil`.
