46224b1de078ca766bd7b302f0226769e1cb1dbe
Andrew Wei <andrew@sybl.io>
2023-03-20 00:42:34 -0700

    Partial commit

---
+++ Sources/BaseKit/functional/Equatable.ext.swift
@@ -0,0 +1,9 @@
+// © GHOZT
+
+extension Equatable {
+  public func isEqual(to other: any Equatable) -> Bool {
+    guard let other = other as? Self else { return false }
+
+    return self == other
+  }
+}
