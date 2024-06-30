1e6e097c9d2af5e7a825a485241d2e721d8de303
Andrew Wei <andrew@sybl.io>
2024-06-30 12:34:06 -0700

    feat: Improve error()

--- Sources/BaseKit/util/debugError.swift
+++ Sources/BaseKit/util/debugError.swift
@@ -1,4 +1,4 @@
 import Foundation
 
 /// Returns a dummy error meant for development use only.
-let debugError: Error = error(domain: "debug", code: -1, description: "This is a dummy error meant for development use only. Please replace this with an actual error.")
+let debugError: Error = error("This is a dummy error meant for development use only. Please replace this with an actual error.", code: -1)
--- Sources/BaseKit/util/error.swift
+++ Sources/BaseKit/util/error.swift
@@ -3,17 +3,17 @@ import Foundation
 /// Creates a generic error.
 ///
 /// - Parameters:
-///   - domain: The domain of the error.
-///   - code: The code of the error.
 ///   - description: The localized description of the error, can be accessed via
 ///                  the `localizedDescription` property of the returned
 ///                  `NSError` object.
+///   - domain: The domain of the error.
+///   - code: The code of the error.
 ///   - reason:The localized failure reason of the error, can be accessed via
 ///            the `localizedFailureReason` property of the returned `NSError`
 ///            object. Defaults to `description`.
 ///
-/// - Returns:The `NSError` object.
-public func error(domain: String, code: Int = 0, description: String = "", reason: String? = nil) -> Error {
+/// - Returns:The `Error` object.
+public func error(_ description: String, domain: String = NSCocoaErrorDomain, code: Int = 0, reason: String? = nil) -> Error {
   NSError(domain: domain, code: code, userInfo: [
     NSLocalizedDescriptionKey: description,
     NSLocalizedFailureErrorKey: reason ?? description,
