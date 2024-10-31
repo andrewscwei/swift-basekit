e95c960eab4d01406a76e710dfa54007d7236414
Andrew Wei <andrewscwei@gmail.com>
2024-10-31 15:05:35 +0900

    feat: Remove debugError

--- Sources/util/debugError.swift
+++
@@ -1,4 +0,0 @@
-import Foundation
-
-/// Returns a dummy error meant for development use only.
-let debugError: Error = error("This is a dummy error meant for development use only. Please replace this with an actual error.", code: -1)
