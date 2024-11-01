e539f3473da6f5dcbf8ed33a3f91a74c874ba9c4
Andrew Wei <andrewscwei@gmail.com>
2024-11-01 11:13:15 +0900

    feat: Create Log struct

--- Sources/util/log.swift
+++ Sources/util/log.swift
@@ -1,52 +1,139 @@
 import os.log
 import Foundation
 
-/// Specifies whether zen mode is enabled for logging. In zen mode, logs are
-/// simplified to improve readability but unified logging is disabled.
-nonisolated(unsafe) public var kZenLogging: Bool = false
+public struct Log: Sendable {
+  /// Logs a message to the unified logging system in the `default` level.
+  ///
+  /// Log level priorities are as follows, from highest to lowest:
+  ///   1. `fault`
+  ///   2. `error`
+  ///   3. `debug`
+  ///   4. `info`
+  ///   5. `default`
+  ///
+  /// - Parameters:
+  ///   - message: The message.
+  ///   - isPublic: Specifies if the log is publicly accessible.
+  ///   - isEnabled: Specifies if the log is enabled, a convenient way to skip
+  ///                this log based on some external flag.
+  ///   - fileName: Name of the file where this function was called.
+  ///   - functionName: Name of the function where this function was called.
+  ///   - lineNumber: Line number where this function was called.
+  public func `default`(_ message: String, isPublic: Bool = true, isEnabled: Bool = true, fileName: String = #file, functionName: String = #function, lineNumber: Int = #line) {
+    log(message, level: .default, isPublic: isPublic, isEnabled: isEnabled, fileName: fileName, functionName: functionName, lineNumber: lineNumber)
+  }
 
-/// Logs a message to the unified logging system.
-///
-/// - Parameters:
-///   - level: The log level.
-///   - isPublic: Specifies if the log is publicly accessible.
-///   - isEnabled: Specifies if the log is enabled, a convenient way to skip
-///                this log based on some external flag.
-///   - fileName: Name of the file where this function was called.
-///   - functionName: Name of the function where this function was called.
-///   - lineNumber: Line number where this function was called.
-///   - message: The block that returns the message.
-public func log(_ level: OSLogType = .info, isPublic: Bool = true, isEnabled: Bool = true, fileName: String = #file, functionName: String = #function, lineNumber: Int = #line, message: () -> String) {
-  if !isEnabled { return }
+  /// Logs a message to the unified logging system in the `info` level.
+  ///
+  /// Log level priorities are as follows, from highest to lowest:
+  ///   1. `fault`
+  ///   2. `error`
+  ///   3. `debug`
+  ///   4. `info`
+  ///   5. `default`
+  ///
+  /// - Parameters:
+  ///   - message: The message.
+  ///   - isPublic: Specifies if the log is publicly accessible.
+  ///   - isEnabled: Specifies if the log is enabled, a convenient way to skip
+  ///                this log based on some external flag.
+  ///   - fileName: Name of the file where this function was called.
+  ///   - functionName: Name of the function where this function was called.
+  ///   - lineNumber: Line number where this function was called.
+  public func info(_ message: String, isPublic: Bool = true, isEnabled: Bool = true, fileName: String = #file, functionName: String = #function, lineNumber: Int = #line) {
+    log(message, level: .info, isPublic: isPublic, isEnabled: isEnabled, fileName: fileName, functionName: functionName, lineNumber: lineNumber)
+  }
+
+  /// Logs a message to the unified logging system in the `debug` level.
+  ///
+  /// Log level priorities are as follows, from highest to lowest:
+  ///   1. `fault`
+  ///   2. `error`
+  ///   3. `debug`
+  ///   4. `info`
+  ///   5. `default`
+  ///
+  /// - Parameters:
+  ///   - message: The message.
+  ///   - isPublic: Specifies if the log is publicly accessible.
+  ///   - isEnabled: Specifies if the log is enabled, a convenient way to skip
+  ///                this log based on some external flag.
+  ///   - fileName: Name of the file where this function was called.
+  ///   - functionName: Name of the function where this function was called.
+  ///   - lineNumber: Line number where this function was called.
+  public func debug(_ message: String, isPublic: Bool = true, isEnabled: Bool = true, fileName: String = #file, functionName: String = #function, lineNumber: Int = #line) {
+    log(message, level: .debug, isPublic: isPublic, isEnabled: isEnabled, fileName: fileName, functionName: functionName, lineNumber: lineNumber)
+  }
+
+  /// Logs a message to the unified logging system in the `error` level.
+  ///
+  /// Log level priorities are as follows, from highest to lowest:
+  ///   1. `fault`
+  ///   2. `error`
+  ///   3. `debug`
+  ///   4. `info`
+  ///   5. `default`
+  ///
+  /// - Parameters:
+  ///   - message: The message.
+  ///   - isPublic: Specifies if the log is publicly accessible.
+  ///   - isEnabled: Specifies if the log is enabled, a convenient way to skip
+  ///                this log based on some external flag.
+  ///   - fileName: Name of the file where this function was called.
+  ///   - functionName: Name of the function where this function was called.
+  ///   - lineNumber: Line number where this function was called.
+  public func error(_ message: String, isPublic: Bool = true, isEnabled: Bool = true, fileName: String = #file, functionName: String = #function, lineNumber: Int = #line) {
+    log(message, level: .error, isPublic: isPublic, isEnabled: isEnabled, fileName: fileName, functionName: functionName, lineNumber: lineNumber)
+  }
+
+  /// Logs a message to the unified logging system in the `fault` level.
+  ///
+  /// Log level priorities are as follows, from highest to lowest:
+  ///   1. `fault`
+  ///   2. `error`
+  ///   3. `debug`
+  ///   4. `info`
+  ///   5. `default`
+  ///
+  /// - Parameters:
+  ///   - message: The message.
+  ///   - isPublic: Specifies if the log is publicly accessible.
+  ///   - isEnabled: Specifies if the log is enabled, a convenient way to skip
+  ///                this log based on some external flag.
+  ///   - fileName: Name of the file where this function was called.
+  ///   - functionName: Name of the function where this function was called.
+  ///   - lineNumber: Line number where this function was called.
+  public func fault(_ message: String, isPublic: Bool = true, isEnabled: Bool = true, fileName: String = #file, functionName: String = #function, lineNumber: Int = #line) {
+    log(message, level: .fault, isPublic: isPublic, isEnabled: isEnabled, fileName: fileName, functionName: functionName, lineNumber: lineNumber)
+  }
+
+  private func log(_ message: String, level: OSLogType = .info, isPublic: Bool, isEnabled: Bool, fileName: String, functionName: String, lineNumber: Int) {
+    guard isEnabled else { return }
 
 #if DEBUG
-  let fileName = fileName.components(separatedBy: "/").last?.components(separatedBy: ".").first
-  let subsystem = Bundle.main.bundleIdentifier ?? "app"
-
-  if kZenLogging {
-    guard level != .default else { return }
-    print(getZenSymbol(for: level), message())
-  }
-  else {
+#if BASEKIT_VERBOSE_LOGGING
+    let fileName = fileName.components(separatedBy: "/").last?.components(separatedBy: ".").first
+    let subsystem = Bundle.main.bundleIdentifier ?? "app"
     let category = "\(fileName ?? "???"):\(lineNumber)"
 
     if isPublic {
-      os_log("%{public}@", log: OSLog(subsystem: subsystem, category: category), type: level, message())
+      os_log("%{public}@", log: OSLog(subsystem: subsystem, category: category), type: level, message)
     }
     else {
-      os_log("%{private}@", log: OSLog(subsystem: subsystem, category: category), type: level, message())
+      os_log("%{private}@", log: OSLog(subsystem: subsystem, category: category), type: level, message)
     }
-  }
+#else
+    guard level != .default else { return }
+    print(getSymbol(for: level), message)
 #endif
+#endif
+  }
 }
 
-/// Returns the logging symbol (in zen mode) of the specified log level.
-///
-/// - Parameters:
-///   - level: The log level.
-///
-/// - Returns: The log symbol.
-private func getZenSymbol(for level: OSLogType) -> String {
+/// Global logger.
+public let log = Log()
+
+private func getSymbol(for level: OSLogType) -> String {
   switch level {
   case .fault: return "💀"
   case .error: return "⚠️"
