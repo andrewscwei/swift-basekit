2bc4fcbb8d0255111b9601c276b76e5e807729aa
Andrew Wei <andrewscwei@gmail.com>
2024-11-04 22:52:18 +0900

    feat: Improve delay function

--- Sources/util/delay.swift
+++ Sources/util/delay.swift
@@ -3,12 +3,30 @@ import Foundation
 /// Suspends execution for the specified number of seconds before resuming.
 ///
 /// - Parameters:
-///   - queue: The dispatch queue to suspend on.
 ///   - seconds: The number of seconds to wait before resuming.
-public func delay(queue: DispatchQueue = .main, _ seconds: TimeInterval) async {
+public func delay(_ seconds: TimeInterval) async {
+  try? await Task.sleep(nanoseconds: UInt64(seconds * 1_000_000_000))
+}
+
+/// Suspends execution for the specified number of seconds before resuming.
+///
+/// - Parameters:
+///   - seconds: The number of seconds to wait before resuming.
+///   - queue: The dispatch queue to suspend on.
+public func delay(_ seconds: TimeInterval, queue: DispatchQueue) async {
   await withCheckedContinuation { continuation in
     queue.asyncAfter(deadline: .now() + seconds) {
       continuation.resume()
     }
   }
+}
+
+/// Executes a closure after a delay in seconds, returning immediately.
+///
+/// - Parameters:
+///   - seconds: The number of seconds to wait before executing the closure.
+///   - queue: The dispatch queue to execute the closure on.
+///   - execute: The closure to execute.
+public func delay(_ seconds: TimeInterval, queue: DispatchQueue = .global(qos: .utility), execute: @escaping @Sendable () -> Void) {
+  queue.asyncAfter(deadline: .now() + seconds, execute: execute)
 }
