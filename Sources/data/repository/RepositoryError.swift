89b4623cab50615eac4963005c6dc4cee2430d85
Andrew Wei <andrewscwei@gmail.com>
2024-11-04 23:21:44 +0900

    feat: Update RepositoryError

--- Sources/data/repository/RepositoryError.swift
+++ Sources/data/repository/RepositoryError.swift
@@ -1,17 +1,39 @@
 /// A type of `Error` thrown by `Repository` types.
 public enum RepositoryError: Error {
 
-  /// An error occurred during sync.
+  /// An error occurred during data synchronization.
+  ///
+  /// - Parameters:
+  ///   - cause: Optional causal `Error`.
   case invalidSync(cause: Error?)
 
-  /// There is an error reading from the datasource.
-  case invalidRead(cause: Error)
+  /// An error occurred while reading from datasource(s).
+  ///
+  /// - Parameters:
+  ///   - cause: Optional causal `Error`.
+  case invalidRead(cause: Error?)
 
-  /// There is an error writing to the datasource.
-  case invalidWrite(cause: Error)
+  /// An error occurred while writing to datasource(s).
+  ///
+  /// - Parameters:
+  ///   - cause: Optional causal `Error`.
+  case invalidWrite(cause: Error?)
 
-  /// There is an error deleting from the datasource.
-  case invalidDelete(cause: Error)
+  /// An error occurred while deleting from datasource(s).
+  ///
+  /// - Parameters:
+  ///   - cause: Optional causal `Error`.
+  case invalidDelete(cause: Error?)
 
+  /// An error occurred during data synchronization.
   public static let invalidSync: RepositoryError = .invalidSync(cause: nil)
+
+  /// An error occurred while reading from datasource(s).
+  public static let invalidRead: RepositoryError = .invalidRead(cause: nil)
+
+  /// An error occurred while writing to datasource(s).
+  public static let invalidWrite: RepositoryError = .invalidWrite(cause: nil)
+
+  /// An error occurred while deleting from datasource(s).
+  public static let invalidDelete: RepositoryError = .invalidDelete(cause: nil)
 }
