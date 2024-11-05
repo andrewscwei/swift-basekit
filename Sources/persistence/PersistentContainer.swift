0c85e339a1a817ec373b404f6f226de76a665a85
Andrew Wei <andrewscwei@gmail.com>
2024-11-06 08:53:34 +0900

    fix: Fix concurrency issues with PersistentContainer

--- Sources/persistence/PersistentContainer.swift
+++ Sources/persistence/PersistentContainer.swift
@@ -1,81 +1,64 @@
 import CoreData
 
-public final class PersistentContainer {
-  let groupIdentifier: String
-  let modelName: String
+public struct PersistentContainer: @unchecked Sendable {
+  public enum Error: Swift.Error {
+    case noStoreCoordinator
+  }
 
-  /// Returns the `NSManagedObjectContext` for the application (which is already
-  /// bound to the persistent store coordinator for the application). This
-  /// property could be optional (though not ideal) since there are legitimate
-  /// error conditions that could cause the creation of the context to fail.
-  private lazy var context: NSManagedObjectContext = {
-    var context = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
-    context.persistentStoreCoordinator = coordinator
+  private let context: NSManagedObjectContext
 
-    return context
-  }()
-
-  /// The `NSPersistentStoreCoordinator` for the application. This lazy property
-  /// creates and returns a coordinator, having added the store for the
-  /// application to it. This property could be optional (though not ideal)
-  /// since there are legitimate error conditions that could cause the creation
-  /// of the store to fail.
-  private lazy var coordinator: NSPersistentStoreCoordinator = {
-    let coordinator = NSPersistentStoreCoordinator(managedObjectModel: model)
-
-    do {
-      try coordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: storeURL, options: [
-        NSInferMappingModelAutomaticallyOption: true,
-        NSMigratePersistentStoresAutomaticallyOption: true,
-      ])
-
-      _log.info("Initializing Core Data for group identifier <\(groupIdentifier)> and model name <\(modelName)>... OK")
-    }
-    catch {
-      _log.error("Initializing Core Data for group identifier <\(groupIdentifier)> and model name <\(modelName)>... ERR: \(error)")
-    }
-
-    return coordinator
-  }()
-
-  /// `URL` to the `CoreData` SQLite file.
-  private lazy var storeURL: URL? = {
-    FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: groupIdentifier)?.appendingPathComponent("\(modelName).sqlite+")
-  }()
-
-  /// `URL` to the `CoreData` model file.
-  private lazy var modelURL: URL? = {
-    Bundle.main.url(forResource: modelName, withExtension: "momd")
-  }()
-
-  /// The `NSManagedObjectModel` for the application.
-  private lazy var model: NSManagedObjectModel = {
-    guard let modelURL = modelURL else {
-      _log.fault("Loading Core Data model... ERR: Unable to evaluate file URL of Core Data model")
-      abort()
-    }
-
-    guard let model = NSManagedObjectModel(contentsOf: modelURL) else {
-      _log.fault("Loading Core Data model... ERR: Unable to create Core Data model at URL \(modelURL)")
-      abort()
-    }
-
-    return model
-  }()
-
-  /// Executes the `block` in the `NSManagedObjectContext`'s private queue.
+  /// Creates a new `PersistenceContainer` instance.
   ///
   /// - Parameters:
-  ///   - block: The `block` to execute.
-  @discardableResult
-  public func perform<T>(block: @escaping (_ context: NSManagedObjectContext, _ coordinator: NSPersistentStoreCoordinator) throws -> T) async throws -> T {
-    try await context.perform { [self] in
-      try block(context, coordinator)
+  ///   - modelName: The model name.
+  ///   - groupIdentifier: The group identifier.
+  public init(modelName: String, groupIdentifier: String) {
+    context = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
+
+    if let coordinator = Self.createCoordinator(modelName: modelName, groupIdentifier: groupIdentifier) {
+      context.persistentStoreCoordinator = coordinator
     }
   }
 
-  public init(modelName: String, groupIdentifier: String) {
-    self.modelName = modelName
-    self.groupIdentifier = groupIdentifier
+  /// Executes the `block` in the `NSManagedObjectContext`'s private queue and
+  /// returns some resulting data.
+  ///
+  /// - Parameters:
+  ///   - block: The closure to execute.
+  /// - Returns: The resulting data.
+  /// - Throws: If persistent store coordinator is invalid or if the block
+  ///           threw.
+  public func perform<T>(block: @escaping (NSManagedObjectContext) throws -> T) async throws -> T {
+    guard context.persistentStoreCoordinator != nil else { throw Error.noStoreCoordinator }
+
+    return try await context.perform {
+      try block(self.context)
+    }
+  }
+
+  private static func createCoordinator(modelName: String, groupIdentifier: String) -> NSPersistentStoreCoordinator? {
+    guard let modelURL = Bundle.main.url(forResource: modelName, withExtension: "momd"), let model = NSManagedObjectModel(contentsOf: modelURL) else {
+      _log.fault("Loading Core Data model with name \(modelName)... ERR: Unable to create Core Data model at URL")
+
+      return nil
+    }
+
+    let coordinator = NSPersistentStoreCoordinator(managedObjectModel: model)
+
+    if let storeURL = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: groupIdentifier)?.appendingPathComponent("\(modelName).sqlite+") {
+      do {
+        try coordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: storeURL, options: [
+          NSInferMappingModelAutomaticallyOption: true,
+          NSMigratePersistentStoresAutomaticallyOption: true,
+        ])
+
+        _log.info("Initializing Core Data for group identifier <\(groupIdentifier)> and model name <\(modelName)>... OK")
+      }
+      catch {
+        _log.error("Initializing Core Data for group identifier <\(groupIdentifier)> and model name <\(modelName)>... ERR: \(error)")
+      }
+    }
+
+    return coordinator
   }
 }
